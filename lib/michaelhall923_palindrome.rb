cf627ec65fcef79717823677259da6b950cb99fb
Michael Hall <michaelhall923@protonmail.com>
2018-12-04 20:21:35 -0500

    Finish working and refactoring palindrome method

--- lib/michaelhall923_palindrome.rb
+++ lib/michaelhall923_palindrome.rb
@@ -1,6 +1,13 @@
 require "michaelhall923_palindrome/version"
 
-module Michaelhall923Palindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
